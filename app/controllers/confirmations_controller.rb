73da7c1af10589b13c8b56428b40b023d075773d
ganeshsakle <ganeshsakle7@gmail.com>
2022-07-15 18:42:52 +0530

    check

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -5,6 +5,6 @@ class ConfirmationsController < Devise::ConfirmationsController
 
   def after_confirmation_path_for(_resource_name, resource)
     sign_in(resource)
-    root_path
+    'posts#index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # get 'friends/new'
-  # get 'friends/edit'
-  # get 'friends/index'
-  # get 'friends/save'
-  # get 'friends/update'
-  # get 'friends/destroy'
-  # get 'comments/new'
-  # get 'comments/edit'
-  # get 'comments/index'
-  # get 'comments/save'
-  # get 'comments/update'
-  # get 'comments/destroy'
-  # get 'posts/new'
-  # get 'posts/edit'
-  # get 'posts/index'
-  # get 'posts/save'
-  # get 'posts/update'
-  # get 'posts/destroy'
+
   root to: 'posts#index'
   get 'home', to: 'static_page#new'
 
-  # root to: 'users#sign_up' #'devise/sessions#new'
-  # get 'profile' , to: 'static_page#index'
-  # get 'profile/:id', to: 'static_page#show'
   get '/static_page/index'
   get '/static_page/show'
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', confirmations: "users/confirmations" }
 
   resources :notifications
   resources :comments
