240b8bf4bc2c542a557e3ab9adf150f5b6b358a0
ganeshsakle <ganeshsakle7@gmail.com>
2022-07-15 11:45:55 +0530

    mailer added in the production mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,20 +75,17 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
-  config.action_mailer.raise_delivery_errors = false
-
+  #replace with your own url
+  config.action_mailer.default_url_options = { host: "localhost:3000"  }
+  #config.action_mailer.default_url_options = { host: 'localhost:3000'}
+  # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-  :address => 'smtp.gmail.com',
-  :port => 601,
-  :user_name => tweetapp4@gmail.com,
-  :password => <gmail_password>,
-  :authentication => 'plain',
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => "tweetapp4@gmail.com",
+  :password             => "baisaqpcyhpyvnil",
+  :authentication       => "plain",
   :enable_starttls_auto => true
 }
-
 end
