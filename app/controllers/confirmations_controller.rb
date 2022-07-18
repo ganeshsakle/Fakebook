57bbbc4ea1ce700de821254f7f44956378b36e74
ganeshsakle <ganeshsakle7@gmail.com>
2022-07-18 13:55:01 +0530

    mailer bug fixed

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -2,9 +2,8 @@
 
 class ConfirmationsController < Devise::ConfirmationsController
   private
-
-  def after_confirmation_path_for(_resource_name, resource)
-    sign_in(resource)
-    root_path
+  def after_confirmation_path_for(resource_name, resource)
+    sign_in(resource) # In case you want to sign in the user
+    user_session_path
   end
 end
