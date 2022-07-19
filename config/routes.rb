52cda005f37f47a1c2857c631714f0a63545c8a2
ganeshsakle <ganeshsakle7@gmail.com>
2022-07-19 12:25:02 +0530

    modify

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {  
     confirmations: 'users/confirmations',
     omniauth_callbacks: 'users/omniauth_callbacks'
+    registations: 'users/registrations'
   }
 
   root to: 'posts#index'
